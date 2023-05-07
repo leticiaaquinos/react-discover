1434781b3080b8d1881c0d40a1638224832d037f
leticiaaquinos <lasarte@outlook.com.br>
2023-05-07 00:03:49 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+function Home() {
+
+  return (
+      <div className="container">
+        <h1>App Lista de Presença</h1>
+        <input placeholder='Digite o nome...'></input>
+        <button>Adicionar</button>
+      </div> 
+  )
+}
+
+export default Home
